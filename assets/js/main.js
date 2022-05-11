cbe247b43f9430e70ada7f15f6fe6aabfc125f94
StephanPagel <stephan.pagel84@gmail.com>
2022-05-11 23:50:14 +0200

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 5;
+
+function sumComp() {
+    result = a + b;
+    if (a == b) {
+        console.log(result * 5);
+    }
+    else {
+        console.log(result);
+    }
+}
+
+sumComp(a, b);
